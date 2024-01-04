5749556c18737aea93a009fc1cf3965906cf8756
Nil Geisweiller <ngeiswei@gmail.com>
2024-01-04 09:18:37 +0200

    Use prefix in example

--- examples/example.py
+++ examples/example.py
@@ -1,6 +1,6 @@
 import example_pb2
 from protobuf_metta import parser
 
-proto_parser = parser.ProtobufParser(example_pb2.DESCRIPTOR)
+proto_parser = parser.ProtobufParser(example_pb2.DESCRIPTOR, prefix="simple")
 metta_desc = proto_parser.description_to_metta()
 print(metta_desc)
