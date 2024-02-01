f6eca965e0b49d88044997d86a72bc31872bc54c
Nil Geisweiller <ngeiswei@gmail.com>
2024-02-01 11:01:14 +0200

    Upgrade example to use curried option

--- examples/example.py
+++ examples/example.py
@@ -1,6 +1,8 @@
 import example_pb2
 from protobuf_metta import parser
 
-proto_parser = parser.ProtobufParser(example_pb2.DESCRIPTOR, prefix="simple")
+proto_parser = parser.ProtobufParser(example_pb2.DESCRIPTOR,
+                                     prefix="simple",
+                                     curried=True)
 metta_desc = proto_parser.description_to_metta()
 print(metta_desc)
